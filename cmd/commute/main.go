7f81531dec64e0c4df193a203825da27e169aea3
Vincent Boutour <bob@vibioh.fr>
2024-04-21 23:12:31 +0200

    feat: Use logger fatal on err
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/commute/main.go
+++ cmd/commute/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/ViBiOh/httputils/v4/pkg/alcotest"
@@ -16,18 +15,16 @@ import (
 )
 
 func main() {
+	ctx := context.Background()
+
 	config, err := newConfig()
-	if err != nil {
-		log.Fatal(fmt.Errorf("config: %s", err))
-	}
+	logger.FatalfOnErr(ctx, err, "config")
 
 	alcotest.DoAndExit(config.alcotest)
 
 	go func() {
 		fmt.Println(http.ListenAndServe("localhost:9999", http.DefaultServeMux))
 	}()
-
-	ctx := context.Background()
 
 	client, err := newClient(ctx, config)
 	logger.FatalfOnErr(ctx, err, "client")
