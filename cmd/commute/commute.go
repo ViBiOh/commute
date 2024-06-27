dc5e9556237963581ed0da9a235f4efbb0145f35
Vincent Boutour <bob@vibioh.fr>
2024-06-27 23:10:24 +0200

    refactor: Splitting main into multiple files
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

---
+++ cmd/commute/commute.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+
+	"github.com/ViBiOh/httputils/v4/pkg/alcotest"
+	"github.com/ViBiOh/httputils/v4/pkg/httputils"
+	"github.com/ViBiOh/httputils/v4/pkg/logger"
+	"github.com/ViBiOh/httputils/v4/pkg/server"
+)
+
+func main() {
+	config := newConfig()
+	alcotest.DoAndExit(config.alcotest)
+
+	ctx := context.Background()
+
+	clients, err := newClients(ctx, config)
+	logger.FatalfOnErr(ctx, err, "client")
+
+	go clients.Start()
+	defer clients.Close(ctx)
+
+	services := newServices(config)
+	port := newPort(config, services)
+
+	go services.server.Start(clients.health.EndCtx(), httputils.Handler(
+		port,
+		clients.health,
+		clients.telemetry.Middleware("http"),
+		services.owasp.Middleware,
+		services.cors.Middleware,
+	))
+
+	clients.health.WaitForTermination(services.server.Done())
+
+	server.GracefulWait(services.server.Done())
+}
--- cmd/commute/services.go
+++ cmd/commute/services.go
@@ -5,21 +5,30 @@ import (
 	"github.com/ViBiOh/commute/pkg/mapbox"
 	"github.com/ViBiOh/commute/pkg/strava"
 	"github.com/ViBiOh/commute/pkg/wahoo"
+	"github.com/ViBiOh/httputils/v4/pkg/cors"
+	"github.com/ViBiOh/httputils/v4/pkg/owasp"
+	"github.com/ViBiOh/httputils/v4/pkg/server"
 )
 
 type services struct {
-	commute commute.Service
+	server  *server.Server
+	cors    cors.Service
 	strava  strava.Service
 	wahoo   wahoo.Service
+	commute commute.Service
+	owasp   owasp.Service
 }
 
 func newServices(config configuration) services {
-	stravaService := strava.New(config.strava, *config.publicURL)
-	wahooService := wahoo.New(config.wahoo, *config.publicURL)
+	var output services
 
-	return services{
-		strava:  stravaService,
-		wahoo:   wahooService,
-		commute: commute.New(*config.publicURL, mapbox.New(config.mapbox), stravaService, wahooService),
-	}
+	output.server = server.New(config.server)
+	output.owasp = owasp.New(config.owasp)
+	output.cors = cors.New(config.cors)
+
+	output.strava = strava.New(config.strava, *config.publicURL)
+	output.wahoo = wahoo.New(config.wahoo, *config.publicURL)
+	output.commute = commute.New(*config.publicURL, mapbox.New(config.mapbox), output.strava, output.wahoo)
+
+	return output
 }
